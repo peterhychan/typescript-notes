098bf23f7717dc7e723a1e86cfa4a07d625243a8
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-12-17 10:08:41 -0800

    type inference err detected

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = "2";
+var n2 = 3;
+var result = add(n1, n2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("Hello World")
+function add(n1: number,n2: number){
+  return n1+n2
+}
+
+const n1="2"
+const n2=3
+const result = add(n1,n2) // tsc helps users during dev i.e. b4 the codes gets compiled
+console.log(result)
