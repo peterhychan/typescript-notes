2adeefbac4f770dd8c982e5091291860b317ea07
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-12-17 15:27:30 -0800

    introduced new types

--- app.ts
+++ app.ts
@@ -1,17 +1,3 @@
-type Combinable = number |string; // alias
-
-function addition(e1: Combinable, e2: Combinable, result: 'as-num'|'as-txt'){
-  let res
-  if(typeof e1==='number'&& e2==='number' || result==='as-num'){
-    res =+e1 + +e2
-  }else{
-    res= e1.toString()+e2.toString()
-  }
-  return res
+function add(n1: number, n2:number): number{
+  return n1+n2
 }
-
-const intAdd = addition(1,2, 'as-num')
-const strAdd = addition("A","V", 'as-txt')
-console.log(intAdd);
-console.log(strAdd)
-console.log(typeof +"2")
