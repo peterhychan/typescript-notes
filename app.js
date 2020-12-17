2a207567f620d02c547c067fef96640160fe36ec
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-12-17 10:12:50 -0800

    JS is dynam-type, while ts is statically-type

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function add(n1, n2) {
-    return n1 + n2;
+    if (typeof n1 === "number" && typeof n2 === "number") {
+        return n1 + n2;
+    }
+    throw new Error("Invalid data type");
 }
 var n1 = "2";
 var n2 = 3;
--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 function add(n1: number,n2: number){
-  return n1+n2
+  if(typeof n1 ==="number" && typeof n2 ==="number"){
+    return n1+n2
+  }
+  throw new Error("Invalid data type")
 }
 
 const n1="2"
 const n2=3
-const result = add(n1,n2) // tsc helps users during dev i.e. b4 the codes gets compiled
+const result = add(n1,n2)
 console.log(result)
