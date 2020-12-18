2de0ab4136acb37388f44d4bbd4b6b0d7106c2f8
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-12-18 11:15:42 -0800

    updated setup && README

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ This is my study notes on learning TypeScript.
 2. Install `lite-server` 
 3. When you update the `.tsc` file, run `npm run start`
 
+
+## Setup watch mode for a tsc file
+- `tsc <TypeScriptFile> -w`
+
+## Settup tsc compilation for the entire folder
+1.  `tsc --init` -> We get a `tsconfig.json`
+2.  `tsc`  
+
+
 TODOS
 1. Add React App Demo
 2. Add NodeJS Demo
--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-function addition(e1, e2, result) {
-    var res;
-    if (typeof e1 === 'number' && e2 === 'number') {
-        res = e1 + e2;
-    }
-    else {
-        result = e1.toString() + e2.toString();
-    }
-    return res;
+"use strict";
+function add(n1, n2) {
+    return n1 + n2;
 }
-var intAdd = addition(1, 2);
-var strAdd = addition("A", "V");
-console.log(intAdd);
-console.log(strAdd);
-console.log(typeof +"2");
